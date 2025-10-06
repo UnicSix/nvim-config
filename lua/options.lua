49010cdcd80f28a9047d542c5b2dde26f410fe44
Noookia <matchman7980@gmail.com>
2025-10-06 22:17:54 +0800

    Update options

--- lua/options.lua
+++ lua/options.lua
@@ -50,6 +50,17 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 0
   end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "comp", "glsl", "hlsl", "frag", "vert", "gemo" },
+  callback = function()
+    vim.opt_local.autoindent = true
+    vim.opt_local.smartindent = true
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.expandtab = true
+    vim.opt_local.softtabstop = 0
+  end,
+})
 --fold nethod
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
