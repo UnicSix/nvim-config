ec31bb304bd4be617e3c06613024ffcdf5560fa7
Noookia <matchman7980@gmail.com>
2025-10-06 22:18:37 +0800

    Update snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -8,17 +8,17 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true },
-		explorer = { enabled = true },
-		indent = { enabled = true },
-		input = { enabled = true },
+		explorer = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = false },
 		picker = { enabled = true },
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
 		scope = { enabled = true },
-		scroll = { enabled = true },
-		statuscolumn = { enabled = true },
-		words = { enabled = true },
-		image = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = false },
+		words = { enabled = false },
+		image = { enabled = false },
 		dashboard = {
 			enabled = true,
 			sections = {
